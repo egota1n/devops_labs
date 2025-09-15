a716694c9138225b11317e8af45f7aadff2e2708
egota1n <rav266@inbox.ru>
2025-09-15 13:31:58 +0300

    Тестирование CD перед защитой

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -3,6 +3,7 @@
       <a-layout-content style="padding: 50px 50px 0 50px">
         <div class="container">
           <a-typography-title :level="2" style="margin-bottom: 30px">Task Manager</a-typography-title>
+          <a-typography-text style="display: block; margin-bottom: 30px">Тестирование</a-typography-text>
           <a-card title="Add/Edit Task" style="margin-bottom: 20px">
             <a-form layout="vertical">
               <a-form-item label="Title">
